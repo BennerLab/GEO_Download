7e83507d501079eb4edd59e66e36b2d2d931a803
sjroth <sjroth@eng.ucsd.edu>
2018-05-01 10:56:38 -0700

    Initial commit

---
+++ Helpers.py
@@ -0,0 +1,79 @@
+'''
+Helper functions for modified fastq-dump.
+'''
+import subprocess
+import sys
+import os
+import shutil
+
+'''
+Define a function that can get the number of spots in a given SRA. This helps with parallelizing runs.
+'''
+def get_num_spots(sra_id):
+
+    p = subprocess.Popen(["sra-stat", "--meta", "--quick", sra_id], stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    txt = stdout.decode().rstrip().split("\n")
+    total = 0
+    for l in txt: total += int(l.split("|")[2].split(":")[0])
+
+    return total
+
+'''
+Define a function that can divide the spots into intervals.
+'''
+def divide_spots(num_spots,num_threads):
+
+    step = num_spots/num_threads
+    intervals = range(0,num_spots+1,step)
+    intervals[-1] = num_spots
+
+    return [[intervals[i]+1,intervals[i+1]] for i in range(len(intervals)-1)]
+
+sra = 'SRR5444627'
+threads = 10
+
+'''
+Define a function that can execute a parallelized fastq-dump. This is essentially a wrapper for fastq-dump that takes 
+advantage of the ability for fastq-dump to dump specific portions of the fastq file.
+'''
+def parallel_dump(sra_id,intervals,paired_end=False,out_dir='',compression='gzip'):
+
+    #Format split and compression command.
+    if paired_end: split_cmd = ['--split-files']
+    else: split_cmd = []
+    if compression: compression_cmd = ['--'+compression]
+    else: compression_cmd = []
+
+    #Begin fastq-dump.
+    if os.path.exists('temp_'+sra_id): shutil.rmtree('temp_'+sra_id)
+    processes = []
+    for i in range(len(intervals)):
+
+        curr_temp_folder = 'temp_'+sra_id+'/'+str(i)
+        cmd = ["fastq-dump","-N",str(intervals[i][0]),"-X",str(intervals[i][1]),"-O",
+               curr_temp_folder]+split_cmd+compression_cmd+[sra_id]
+        p = subprocess.Popen(cmd)
+        processes.append(p)
+
+    for i in range(len(intervals)):
+
+        exit_code = processes[i].wait()
+        if exit_code != 0:
+            sys.stderr.write("fastq-dump error! exit code: {}\n".format(exit_code))
+            sys.exit(1)
+
+    #Join files and remove temporary files.
+    if not os.path.exists(out_dir): os.mkdir(out_dir)
+    if out_dir == '.': out_dir = ''
+    if compression == 'gzip': extension = '.gz'
+    elif compression == 'bzip2': extension = '.bz2'
+    else: extension = ''
+
+    if paired_end: 
+        subprocess.call("cat temp_"+sra_id+"/*/"+sra_id+"_1.* > "+out_dir+sra_id+"_1.fastq"+extension,shell=True)
+        subprocess.call("cat temp_"+sra_id+"/*/"+sra_id+"_2.* > "+out_dir+sra_id+"_2.fastq"+extension, shell=True)
+
+    else: subprocess.call("cat temp_"+sra_id+"/*/"+sra_id+"* > "+out_dir+sra_id+".fastq"+extension,shell=True)
+
+    shutil.rmtree('temp_'+sra_id)
---
+++ modified_fastq_dump.py
@@ -0,0 +1,42 @@
+'''
+Script for performing a modified fastq-dump. Can do this for batches of files.
+'''
+from Helpers import *
+import argparse
+
+def main():
+
+    #Get command line arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s","--sra",help='SRA ID',action="append")
+    parser.add_argument("-n","--num-cpu",help="Number of CPUs",type=int,default=1)
+    parser.add_argument('-o',"--outdir",help='Output Directory',default=".")
+    parser.add_argument('--paired_end',help='Paired End Experiment',action="store_true")
+    parser.add_argument('-c',"--compression",help="Compression Method",default=None)
+    parser.add_argument('--scratch',help='Scratch Location',default=None)
+
+    args = parser.parse_known_args()[0]
+
+    #Run the fastq-dump.
+    if args.sra:
+
+        for sra in args.sra:
+
+            #Get metadata.
+            sys.stdout.write("Getting SRA metadata for "+sra)
+            num_spots = get_num_spots(sra)
+            intervals = divide_spots(num_spots,args.num_cpu)
+
+            #Run the fastq-dump.
+            sys.stdout.write("Performing parallelized fastq-dump")
+            parallel_dump(sra,intervals,args.paired_end,args.outdir,args.compression)
+
+        #Clear the scratch if specified.
+        if args.scratch: shutil.rmtree(args.scratch)
+
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
